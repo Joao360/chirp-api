d3d976f2790efe6d18e9b4ce48505ff1d4cd0512
João Graça <joaograca360@gmail.com>
2026-01-05 12:26:43 +0000

    fix: lua script

--- user/src/main/resources/email_rate_limit.lua
+++ user/src/main/resources/email_rate_limit.lua
@@ -12,7 +12,7 @@ currentCount = currentCount and tonumber(currentCount) or 0
 local newCount = redis.call('INCR', attemptCountKey)
 
 local backoffSeconds = { 60, 300, 3600 } -- 1 minute, 5 minutes, 1 hour
-local backoffIndex = max.min(currentCount, 2) + 1
+local backoffIndex = math.min(currentCount, 2) + 1
 
 redis.call('SETEX', rateLimitKey, backoffSeconds[backoffIndex], '1') -- Set rate limit key with backoff TTL
 redis.call('EXPIRE', attemptCountKey, 86400) -- Set attempt count key to expire in 24 hours
