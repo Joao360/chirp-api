c43f1cabbfd3ef67a0bca080791f63f9af231b72
João Graça <joaograca360@gmail.com>
2026-02-02 12:44:46 +0000

    feat: support transactions in message queues

--- common/src/main/kotlin/com/joaograca/chirp/infra/message_queue/RabbitMqConfig.kt
+++ common/src/main/kotlin/com/joaograca/chirp/infra/message_queue/RabbitMqConfig.kt
@@ -6,18 +6,22 @@ import org.springframework.amqp.core.Binding
 import org.springframework.amqp.core.BindingBuilder
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.amqp.support.converter.JacksonJavaTypeMapper
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.annotation.EnableTransactionManagement
 import tools.jackson.databind.DefaultTyping
 import tools.jackson.databind.json.JsonMapper
 import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator
 import tools.jackson.module.kotlin.kotlinModule
 
 @Configuration
+@EnableTransactionManagement
 class RabbitMqConfig {
 
     @Bean
@@ -36,6 +40,18 @@ class RabbitMqConfig {
 
         return JacksonJsonMessageConverter(objectMapper).apply {
             typePrecedence = JacksonJavaTypeMapper.TypePrecedence.TYPE_ID
+        }
+    }
+
+    @Bean
+    fun rabbitListenerContainerFactory(
+        connectionFactory: ConnectionFactory,
+        transactionManager: PlatformTransactionManager
+    ): SimpleRabbitListenerContainerFactory {
+        return SimpleRabbitListenerContainerFactory().apply {
+            setConnectionFactory(connectionFactory)
+            setTransactionManager(transactionManager)
+            setChannelTransacted(true)
         }
     }
 
--- notification/src/main/kotlin/com/joaograca/chirp/infra/message_queue/NotificationUserEventListener.kt
+++ notification/src/main/kotlin/com/joaograca/chirp/infra/message_queue/NotificationUserEventListener.kt
@@ -3,11 +3,13 @@ package com.joaograca.chirp.infra.message_queue
 import com.joaograca.chirp.domain.events.user.UserEvent
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class NotificationUserEventListener {
 
     @RabbitListener(queues = [MessageQueues.NOTIFICATION_USER_EVENTS])
+    @Transactional
     fun handleUserEvent(event: UserEvent) {
         when (event) {
             is UserEvent.Created -> {
