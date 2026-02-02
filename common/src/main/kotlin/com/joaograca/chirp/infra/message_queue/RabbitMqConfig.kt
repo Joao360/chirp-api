841faa6420d16dcac05960b84d3983b2ee36e3a8
João Graça <joaograca360@gmail.com>
2026-02-02 12:39:09 +0000

    feat: listen for user events

--- common/src/main/kotlin/com/joaograca/chirp/infra/message_queue/RabbitMqConfig.kt
+++ common/src/main/kotlin/com/joaograca/chirp/infra/message_queue/RabbitMqConfig.kt
@@ -2,6 +2,8 @@ package com.joaograca.chirp.infra.message_queue
 
 import com.joaograca.chirp.domain.events.ChirpEvent
 import com.joaograca.chirp.domain.events.user.UserEventConstants
+import org.springframework.amqp.core.Binding
+import org.springframework.amqp.core.BindingBuilder
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.core.TopicExchange
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
@@ -59,4 +61,15 @@ class RabbitMqConfig {
         /* name = */ MessageQueues.NOTIFICATION_USER_EVENTS,
         /* durable = */ true
     )
+
+    @Bean
+    fun notificationUserEventsBinding(
+        notificationUserEventsQueue: Queue,
+        userExchange: TopicExchange
+    ): Binding {
+        return BindingBuilder
+            .bind(notificationUserEventsQueue)
+            .to(userExchange)
+            .with("user.*")
+    }
 }
