8a27afa32b818dd6c29d55ee1ee0a2517130bdb9
João Graça <joaograca360@gmail.com>
2026-02-14 16:05:30 +0000

    feat: setup chat entities

---
+++ chat/src/main/kotlin/com/joaograca/chirp/infra/database/entities/ChatParticipantEntity.kt
@@ -0,0 +1,28 @@
+package com.joaograca.chirp.infra.database.entities
+
+import com.joaograca.chirp.domain.type.UserId
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import java.time.Instant
+
+@Entity
+@Table(
+    name = "chat_participants",
+    schema = "chat_service",
+    indexes = [
+        Index(name = "idx_chat_participants_username", columnList = "username"),
+        Index(name = "idx_chat_participants_email", columnList = "email")
+    ]
+)
+class ChatParticipantEntity(
+    @Id
+    var userId: UserId,
+    @Column(nullable = false, unique = true)
+    var username: String,
+    @Column(nullable = false, unique = true)
+    var email: String,
+    @Column(nullable = true)
+    var profilePictureUrl: String? = null,
+    @CreationTimestamp
+    var createdAt: Instant = Instant.now(),
+)
